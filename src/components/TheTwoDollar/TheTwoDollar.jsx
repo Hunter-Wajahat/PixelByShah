6f03b374bb91cd69a94d4dab943171302ee4d731
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-07-29 12:19:06 +0500

    new component for 2 dollar story

---
+++ src/components/TheTwoDollar/TheTwoDollar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './twoDollar.css'
+const TheTwoDollar = () => {
+  return (
+    <div className='storyConainer'>
+     <p>Story: </p> <h1>The 2$ Logo</h1>
+    </div>
+  )
+}
+
+export default TheTwoDollar
